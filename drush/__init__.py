d8365651fe710b7a6c147f50b44b29c512a19e92
David Snopek <dsnopek@gmail.com>
2013-08-30 18:45:32 +0100

    * Re-organized the finding and checking of Drush versions.
     * Put in a little boilerplate for unit testing.

--- drush/__init__.py
+++ drush/__init__.py
@@ -17,46 +17,109 @@ class DrushVersionException(Exception):
     pass
 
 class Drush(object):
-    """An interface to the Drush command. Depends on Drush 6."""
+    """An interface to the Drush command-line program - requires Drush 6 or later."""
 
-    def __init__(self, command=None, alias=None):
-        self.command = self.__find_drush(command)
-        self.__check_alias(alias)
-        self.alias = alias
+    def __init__(self, drush=None, alias=None, verify=True, load_command_methods=True):
+        if drush is None:
+            if verify:
+                drush = ['drush6','drush']
+            else:
+                drush = 'drush'
 
-    # TODO: Use https://code.google.com/p/which/ to find the full command name.
-    def __find_drush(self, command):
-        """Takes a command name and verifies that it's a valid Drush 6 (or later) executable.
-           Returns the command name (possibly modified) which will execute correctly."""
-
-        if command is None:
-            for possibility in ['drush6', 'drush']:
-                try:
-                    path = self.__find_drush(possibility)
-                    break
-                except DrushNotFoundException:
-                    path = None
-
-            if path is not None:
-                return path
-
+        if verify:
+            (self.__drush, self.__version_string, self.__version) = self.__find_drush(drush)
         else:
-            # Attempt to run the command and verify it's version
-            popen = subprocess.Popen([command, '--version'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            popen.wait()
-            if popen.returncode == 0:
-                version = popen.stdout.read()
-                if version is not None:
-                    # This regex will work for Drush 4, 5 and 6
-                    match = re.search(r'[Dd]rush [Vv]ersion\s+(?::\s+)?([\d\.]+)', version)
-                    if match is not None:
-                        parts = match.group(1).split('.')
-                        if int(parts[0]) < 6:
-                            raise DrushVersionException('Drush 6.x or higher is required. Found: ' + version)
-                        return command
+            if type(drush) is list:
+                drush = drush[0]
+            self.__drush = drush
+            self.__version_string = None
+            self.__version = None
+
+        self.__has_command_methods = load_command_methods
+        self.__set_alias(alias)
+
+    @property
+    def _drush(self):
+        return self.__drush
+
+    @property
+    def _version(self):
+        return self.__version
+
+    @property
+    def _version_string(self):
+        return self.__version_string
+
+    @property
+    def _alias(self):
+        return self.__alias
+
+    @property
+    def has_command_methods(self):
+        return self.__has_command_methods
+
+    def __get_drush_version(self, drush):
+        """Attempts to run drush (via the given command) in order to get it's version.
+        Returns a tuple containing the version string and an list of integers."""
+
+        # Attempt to run the command and verify it's version
+        popen = subprocess.Popen([drush, '--version'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        popen.wait()
+        if popen.returncode == 0:
+            output = popen.stdout.read()
+            if output is not None:
+                return self.__parse_drush_version(output)
+
+        return (None, None)
+
+    def __parse_drush_version(self, output):
+        # This regex will work for Drush 4, 5 and 6
+        match = re.search(r'[Dd]rush [Vv]ersion\s+(?::\s+)?([\d\.]+)', output)
+        if match is not None:
+            version_string = match.group(1)
+            version = [int(x) for x in version_string.split('.')]
+            return (version_string, version)
+
+        return (None, None)
+
+    def __find_drush(self, possibilities):
+        """Takes a list of possible drush commands and tries run them each in turn. The first to work
+        and return valid version will be used. Returns a tuple containing the command, version string and
+        version list."""
+
+        if type(possibilities) is not list:
+            possibilities = [possibilities]
+
+        for drush in possibilities:
+            version_string, version = self.__get_drush_version(drush)
+            if version is not None:
+                if version[0] >= 6:
+                    return (drush, version_string, version)
+                else:
+                    raise DrushVersionException('Drush 6.x or higher is required. Found: ' + version_string)
 
         raise DrushNotFoundException('Unable to find Drush command')
 
-    def __check_alias(self, alias):
+    def __set_alias(self, alias):
+        # TODO: verify that this is a valid alias (or None)
+        
+        self.__alias = alias
+
+        self.__load_command_methods()
+
+    def __call__(self, command, args, opts):
         pass
+
+    def __load_command_methods(self):
+        pass
+
+    def site_set(self, site):
+        self.__set_alias(site)
+
+class DrushCommand(object):
+    """A callable object which executes a specific Drush command."""
+
+    def __init__(self, info):
+        pass
+
 
---
+++ tests/test_drush.py
@@ -0,0 +1,22 @@
+
+import os
+import unittest
+
+#import mock
+#from mock import MagicMock, sentinel
+
+from drush import Drush
+
+class DrushTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
